cba57b6d704768c43455954a29f3063b15ce6abd
StellarScript <ej92@protonmail.com>
2024-07-26 19:45:37 -0700

    update 2d page

---
+++ apps/client/src/app/app/2d/components/Canvas/Canvas.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export const CymaticCanvas: React.FC = () => {
+  return <></>;
+};
--- apps/client/src/app/app/2d/page.tsx
+++ apps/client/src/app/app/2d/page.tsx
@@ -1,22 +1,12 @@
 'use server';
-import { Slider } from '@client/components/Slider';
+
+import { CymaticCanvas } from './components/Canvas';
 
 export default async function Visulaization2D() {
   return (
     <div className="flex w-full flex-col items-center">
       <div className="flex w-full flex-row justify-center">
-        <div className="flex w-1/2 flex-col items-start">
-          <div className="flex items-center justify-center text-white">
-            <div className="w-80 rounded-lg bg-gray-900 p-6 shadow-lg">
-              <Slider label="" max={16} initialValue={0} />
-              <Slider label="" max={16} initialValue={0} />
-              <Slider label="" max={16} initialValue={0} />
-            </div>
-          </div>
-        </div>
-        <div className="flex w-1/2 flex-col items-start">
-          <div className="border border-black p-5"></div>
-        </div>
+        <CymaticCanvas />
       </div>
     </div>
   );
