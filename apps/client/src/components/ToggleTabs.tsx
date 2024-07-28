81672dd7e4c79c4a33d2670fe7c4a188fecec68f
StellarScript <ej92@protonmail.com>
2024-07-27 21:29:02 -0700

    update toggle tabs component

--- apps/client/src/components/ToggleTabs.tsx
+++ apps/client/src/components/ToggleTabs.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@client/lib/cn';
 import { useState } from 'react';
 
 interface ToggleTabsProps {
@@ -19,16 +20,18 @@ export const ToggleTabs: React.FC<ToggleTabsProps> = ({ onChange, firstTabLabel,
          <label className="shadow-card relative inline-flex cursor-pointer select-none items-center justify-center rounded-md bg-white p-1">
             <input type="checkbox" className="sr-only" checked={isChecked} onChange={handleCheckboxChange} />
             <span
-               className={`flex items-center space-x-[6px] rounded py-2 px-[18px] text-sm font-medium ${
-                  !isChecked ? 'text-primary bg-[#f4f7ff]' : 'text-body-color'
-               }`}
+               className={cn(
+                  'flex items-center space-x-[6px] rounded rounded-r-none py-2 px-[18px] text-xs font-medium',
+                  !isChecked ? 'text-dark-100 bg-light-200' : 'bg-primary-500 text-white-100',
+               )}
             >
                {firstTabLabel}
             </span>
             <span
-               className={`flex items-center space-x-[6px] rounded py-2 px-[18px] text-sm font-medium ${
-                  isChecked ? 'text-primary bg-[#f4f7ff]' : 'text-body-color'
-               }`}
+               className={cn(
+                  'flex items-center space-x-[6px] rounded rounded-l-none py-2 px-[18px] text-xs font-medium',
+                  isChecked ? 'text-dark-100 bg-light-200' : 'bg-primary-500 text-white-100',
+               )}
             >
                {secondTabLabel}
             </span>
