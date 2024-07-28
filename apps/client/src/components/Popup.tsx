e6e2866630fd737f040bb25a8702c29ce52adb9c
StellarScript <ej92@protonmail.com>
2024-07-28 02:45:47 -0700

    setup popup component

---
+++ apps/client/src/components/Popup.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@client/lib/cn';
+
+interface PopupProps extends React.PropsWithChildren {
+   className?: string;
+   content: string;
+}
+
+export const Popup: React.FC<PopupProps> = ({ children, className, content }) => {
+   return (
+      <div className="group">
+         <div className="relative w-full">
+            <div
+               className={cn(
+                  'absolute invisible group-hover:visible border rounded',
+                  'bg-[#0C111D] w-[14rem] mt-[0.5rem] ml-3 border-dark-100 px-3 py-1 text-dark-100',
+                  className,
+               )}
+            >
+               <div className="tooltip-arrow text-white-100 text-[10px]">{content}</div>
+            </div>
+         </div>
+         {children}
+      </div>
+   );
+};
