7845d516f06874520938c42b08a01b243c5328c2
StellarScript <ej92@protonmail.com>
2024-07-27 23:03:28 -0700

    setup 3d page

--- apps/client/src/app/app/3d/page.tsx
+++ apps/client/src/app/app/3d/page.tsx
@@ -1,5 +1,15 @@
 'use server';
 
+import dynamic from 'next/dynamic';
+
+const CymaticCanvas = dynamic(() => import('./components/Canvas').then((C) => C.CymaticCanvas), {
+   ssr: false,
+});
+
 export default async function Visulaization3D() {
-  return <></>;
+   return (
+      <div className="w-full overflow-auto scroll-smooth">
+         <CymaticCanvas />
+      </div>
+   );
 }
